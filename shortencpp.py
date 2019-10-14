e808386d76a8356136b7cc23697684932bc55969
Zachary Cutlip <uid000@gmail.com>
2019-10-14 13:24:26 -0700

    remove python 2 compatibility imports

--- shortencpp.py
+++ shortencpp.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python
-
-from __future__ import absolute_import
-from __future__ import print_function
-from builtins import range
-from builtins import object
 import re
 import sys
 
